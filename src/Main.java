4feb512b3b6b80a96a283e1b82ad811f7ff0ea47
USUARIO <caballero.heiner@iemodeliaibague.edu.co>
2024-06-04 17:15:59 -0500

    segunda parte mostrar edad y grado con arreglos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num;
+        num = Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese la cantidad de estudiantes"));
+        Scanner entrada = new Scanner(System.in);
+        int edad[];
+        int grado[];
+        int estu[];
+        int estudi[];
+        edad = new int[num];
+        grado = new int[num];
+        estudi = new int[num];
+        estu = new int[num];
+
+        for (int i = 0; i < num; i++) {
+
+            System.out.println((i + 1) + "porfavor digite la edad");
+            edad[i] = entrada.nextInt();
+
+        }
+        for (int i = 0; i < num; i++) {
+            System.out.println((i + 1) + "porfavor ingrese el grado");
+            grado[i] = entrada.nextInt();
+        }
+        int j = 0;
+        for (int i = 0; i < num; i++) {
+            estu[j] = edad[i];
+            j++;
+        }
+        System.out.println(" edad :");
+        for (int i = 0; i < num; i++) {
+            System.out.println(estu[i] + " ");
+        }
+        int g = 0;
+        for (int i = 0; i < num; i++) {
+            estudi[g] = grado[i];
+            g++;
+        }
+        System.out.println("grado :");
+        for (int i = 0; i < num; i++) {
+            System.out.println(estudi[i] + " ");
+        }
+        System.out.println();
+        System.out.println("gracias por ingresar a nuestro programa");
+        for (int i = 0; i < num; i++) {
+            System.out.println("edad : " + estu[i] + "grado :" + estudi[i]);
+        }
     }
 }
+
+
--- src/ninos.java
+++ src/ninos.java
@@ -6,50 +6,42 @@ public class ninos {
         int  num;
         num = Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese la cantidad de estudiantes"));
         Scanner entrada = new Scanner(System.in);
-        float edad [] = new float [num];
-
-       float suma_mayor = 0 , suma_menor = 0 ,media_mayor , media_menor;
-       int conteo_mayor = 0 , conteo_menor = 0 , conteo_igual = 0;
+        int edad[];
+        int grado[];
+        int estu[];
+        int estudi[],conteo_igual = 0,sum_mayor =0,conteo_mayor = 0;
+        edad = new int[num];
+        grado = new int[num];
+        estudi = new int[num];
+        estu = new int[num];
 
         System.out.println("espere porfavor estamos guardando los datos");
+        for (int i = 0 ; i < num; i++) {
+            System.out.println((i + 1) + "porfavor ingrese el grado");
+            grado[i] = entrada.nextInt();
+        }
+   
        for (int i = 0; i < num ; i++) {
 
            System.out.println((i + 1) + "porfavor digite la edad");
-           edad[i] = entrada.nextFloat();
-
-           if (edad[i] == 5) {
-               conteo_igual++;
-           }
-            if (edad[i] >= 5 ) {
-               suma_mayor += edad[i];
-               conteo_mayor++;
-           }
-            if (edad[i] < 5 ){
-               suma_menor += edad[i];
-               conteo_menor++;
-           }
+           edad[i] = entrada.nextInt();
 
        }
-       if (conteo_igual == 5){
-           System.out.println("no se puede saber la cantidad de niños que son menores de 5 años");
-       }
-       else {
-           System.out.println("los niños que tienen 5 años son: " + conteo_igual);
-       }
-       if (conteo_mayor == 5){
-           System.out.println("no se puede saber la cantidad de niños que son menores de 5 años");
-       }
-       else {
-           media_mayor = suma_mayor / conteo_mayor;
-           System.out.println("la media de los  niños que son mayores a 5 son: " + media_mayor);
 
+       int g=0;
+       for(int i=0;i<num;i++){
+           estudi[g] = edad[i];
+           g++;
        }
-if (conteo_menor == 5){
-    System.out.println("no se puede saber la cantidad de niños que son menores de 5 años");
-}
-else {
-    media_menor = suma_menor / conteo_menor;
-    System.out.println("niños que son menores de 5 años son: " + media_menor);
-}
+       int j= 0;
+       for (int i=0;i<num;i++){
+           estu[j]= grado[i];
+           j++;
+       }
+        System.out.println("estudiantes");
+       for (int i = 0 ; i<num; i++){
+           System.out.println("Grado : " + estu[i] + " Edad :" + estudi[i]);
+       }
+        }
     }
-}
+
